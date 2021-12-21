a4ba224744fbf2c2a5bd23c8204e97d96d9d1404
maarcossl <laramarcos59@gmail.com>
2021-12-21 23:52:07 +0100

    Creacion de clase Player y modificacion del main.

--- PacmanParte1/main.cpp
+++ PacmanParte1/main.cpp
@@ -1,4 +1,5 @@
 #include "Map.h"
+#include "Player.h"
 
 /// <summary>
 /// Sets the needed variables
@@ -17,12 +18,9 @@ void Logic();
 /// </summary>
 void Draw();
 
-enum USER_INPUTS { NONE, UP, DOWN, RIGHT, LEFT, QUIT };
+
 Map pacman_map = Map();
-char player_char = 'O';
-int player_x = 1;
-int player_y = 1;
-int player_points = 0;
+Player playeR = Player(pacman_map.spawn_player);
 USER_INPUTS input = USER_INPUTS::NONE;
 bool run = true;
 bool win = false;
@@ -41,8 +39,8 @@ int main()
 void Setup()
 {
     std::cout.sync_with_stdio(false);
-    player_x = pacman_map.spawn_player.X;
-    player_y = pacman_map.spawn_player.Y;
+    srand(time(NULL));
+
 }
 
 void Input()
@@ -83,52 +81,11 @@ void Logic()
     }
     else
     {
-        int player_y_new = player_y;
-        int player_x_new = player_x;
-        switch (input)
-        {
-        case UP:
-            player_y_new--;
-            break;
-        case DOWN:
-            player_y_new++;
-            break;
-        case RIGHT:
-            player_x_new++;
-            break;
-        case LEFT:
-            player_x_new--;
-            break;
-        case QUIT:
+        if (input == USER_INPUTS::QUIT) {
             run = false;
-            break;
         }
-        if (player_x_new < 0)
-        {
-            player_x_new = pacman_map.Width - 1;
-        }
-        player_x_new %= pacman_map.Width;
-        if (player_y_new < 0)
-        {
-            player_y_new = pacman_map.Height - 1;
-        }
-        player_y_new %= pacman_map.Height;
+        playeR.update(&pacman_map, input/*, &enemigos*/);
 
-        switch (pacman_map.GetTile(player_x_new, player_y_new))
-        {
-        case Map::MAP_TILES::MAP_WALL:
-            player_y_new = player_y;
-            player_x_new = player_x;
-            break;
-        case Map::MAP_TILES::MAP_POINT:
-            pacman_map.points--;
-            player_points++;
-            pacman_map.SetTile(player_x_new, player_y_new, Map::MAP_TILES::MAP_EMPTY);
-            break;
-        }
-
-        player_y = player_y_new;
-        player_x = player_x_new;
         if (pacman_map.points <= 0)
         {
             win = true;
@@ -140,12 +97,10 @@ void Draw()
 {
     ConsoleUtils::Console_SetPos(0,0);
     pacman_map.Draw();
-    ConsoleUtils::Console_SetPos(player_x, player_y);
-    ConsoleUtils::Console_SetColor(ConsoleUtils::CONSOLE_COLOR::DARK_YELLOW);
-    std::cout << player_char;
+    playeR.draw();
     ConsoleUtils::Console_ClearCharacter({ 0,(short)pacman_map.Height });
     ConsoleUtils::Console_SetColor(ConsoleUtils::CONSOLE_COLOR::CYAN);
-    std::cout << "Puntuacion actual: " << player_points << " Puntuacion pendiente: " << pacman_map.points << std::endl;
+    std::cout << "Puntuacion actual: " << playeR.points << " Puntuacion pendiente: " << pacman_map.points << std::endl;
     if (win)
     {
         ConsoleUtils::Console_SetColor(ConsoleUtils::CONSOLE_COLOR::GREEN);
