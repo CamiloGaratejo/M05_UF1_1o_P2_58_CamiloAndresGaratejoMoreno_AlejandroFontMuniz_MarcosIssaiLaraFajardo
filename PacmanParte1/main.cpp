0fc7d93629dfc096a4ce6d4fba7d0d6bf6e4851b
Alejandro Font <fontalejandro0@gmail.com>
2021-12-22 00:08:13 +0100

    Cerre la rama de Time Manage

--- PacmanParte1/main.cpp
+++ PacmanParte1/main.cpp
@@ -1,6 +1,5 @@
 #include "Map.h"
 #include "Player.h"
-#include "TimeManager.h"
 
 /// <summary>
 /// Sets the needed variables
@@ -107,9 +106,4 @@ void Draw()
         ConsoleUtils::Console_SetColor(ConsoleUtils::CONSOLE_COLOR::GREEN);
         std::cout << "Has ganado!" << std::endl;
     }
-    std::cout << "Fotogramas: " << TimeManager::getInstance().frameCount << std::endl;
-    std::cout << "Time: " << TimeManager::getInstance().time << std::endl;
-    std::cout << "DeltaTime: " << TimeManager::getInstance().deltatime << std::endl;
-
-    TimeManager::getInstance().NextFrame();
 }
